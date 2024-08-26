5c562f6e5713e21fe938621df6d02ba2b1e8fc9c
Sergio Rodriguez Gundin <sergio.gundin.rodriguez@gmail.com>
2024-08-26 18:25:16 +0200

    defined projects as a content collection

---
+++ src/content/config.ts
@@ -0,0 +1,22 @@
+import { defineCollection, z } from 'astro:content';
+
+const projectsCollection = defineCollection({
+  type: 'data',
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    images: z.array(z.string()),
+    company: z.string(),
+    milestones: z.array(z.string()),
+    stack: z.array(z.object({
+      name: z.string(),
+      icon: z.string(),
+    })),
+    url: z.string().url(),
+    color: z.string(),
+  }),
+});
+
+export const collections = {
+  projects: projectsCollection,
+};
