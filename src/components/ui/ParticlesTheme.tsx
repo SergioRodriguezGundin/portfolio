921af0ca11e5d2b9e2aef4113b7ec04e3a7c2b60
Sergio Rodriguez Gundin <sergio.gundin.rodriguez@gmail.com>
2024-09-09 23:43:58 +0200

    particles dark and light theme

---
+++ src/components/ui/ParticlesTheme.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+import Particles from "@components/magicui/particles";
+
+const ParticlesTheme = () => {
+  const [color, setColor] = useState("#ffffff");
+
+  useEffect(() => {
+    const handleThemeChange = () => {
+      const theme = document.documentElement.getAttribute('data-theme');
+      console.log(theme);
+      setColor(theme === "dark" ? "#ffffff" : "#000000");
+    };
+
+    handleThemeChange();
+    const observer = new MutationObserver(handleThemeChange);
+    observer.observe(document.documentElement, { attributes: true, attributeFilter: ['data-theme'] });
+
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
+
+  return (
+    <Particles
+      className="particles"
+      quantity={100}
+      ease={80}
+      color={color}
+      refresh
+    />
+  );
+};
+
+export default ParticlesTheme;
