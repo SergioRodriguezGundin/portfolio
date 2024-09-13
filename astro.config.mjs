e058138d0afbf187f54e8e3e06bc09fc246f17f3
Sergio Rodriguez Gundin <sergio.gundin.rodriguez@gmail.com>
2024-09-13 09:43:05 +0200

    added astro files to scan css clases

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import react from '@astrojs/react';
 
 import tailwind from '@astrojs/tailwind';
 
+/**
+ * @type {import('astro').AstroConfig}
+ */
 export default defineConfig({
   integrations: [react(), tailwind({
     config: {
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,8 @@
 module.exports = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    './src/**/*.{js,jsx,ts,tsx,astro}',
   ],
-  prefix: "",
   theme: {
     container: {
       center: true,
