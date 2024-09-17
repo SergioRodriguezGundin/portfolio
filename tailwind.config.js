4c15083994508c571f0799c1102d20befa5adfb6
Sergio Rodriguez Gundin <sergio.gundin.rodriguez@gmail.com>
2024-09-17 22:01:32 +0200

    added font sizes with clamp in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,6 +22,15 @@ module.exports = {
       fontFamily: {
         "prompt": ['Prompt', 'sans-serif'],
       },
+      fontSize: {
+        'sm': 'var(--font-size-sm, clamp(0.7rem, 0vi + 0.7rem, 0.7rem))',
+        'base': 'var(--font-size-base, clamp(0.88rem, 0.06vi + 0.86rem, 0.94rem))',
+        'md': 'var(--font-size-md, clamp(1.09rem, 0.16vi + 1.05rem, 1.25rem))',
+        'lg': 'var(--font-size-lg, clamp(1.37rem, 0.29vi + 1.29rem, 1.66rem))',
+        'xl': 'var(--font-size-xl, clamp(1.71rem, 0.5vi + 1.58rem, 2.21rem))',
+        '2xl': 'var(--font-size-2xl, clamp(2.14rem, 0.81vi + 1.93rem, 2.93rem))',
+        '3xl': 'var(--font-size-3xl, clamp(2.67rem, 1.25vi + 2.36rem, 3.9rem))',
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
