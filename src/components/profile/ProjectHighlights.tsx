0d9cd8b272f4062813282ed2c25a224f8e227522
Sergio-RG <40829363+SergioRodriguezGundin@users.noreply.github.com>
2024-10-30 00:50:00 +0100

    adding padding in projects

--- src/components/profile/ProjectHighlights.tsx
+++ src/components/profile/ProjectHighlights.tsx
@@ -1,7 +1,7 @@
 import Marquee from '@components/magicui/marquee';
 
 const HighlightIcon = () => {
-  return <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 1024 1024"><path fill="hsl(var(--primary))" d="M695.4 164.1H470.8L281.2 491.5h157.4l-60.3 241l319.8-305.1h-211z"/><path fill="hsl(var(--color-700))" d="M848.1 359.3H627.8L825.9 109c4.1-5.3.4-13-6.3-13H436.1c-2.8 0-5.5 1.5-6.9 4L170.1 547.5c-3.1 5.3.7 12 6.9 12h174.4L262 917.1c-1.9 7.8 7.5 13.3 13.3 7.7L853.6 373c5.2-4.9 1.7-13.7-5.5-13.7M378.3 732.5l60.3-241H281.2l189.6-327.4h224.6L487.1 427.4h211z"/></svg>;
+  return <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 1024 1024"><path fill="hsl(var(--primary))" d="M695.4 164.1H470.8L281.2 491.5h157.4l-60.3 241l319.8-305.1h-211z" /><path fill="hsl(var(--color-700))" d="M848.1 359.3H627.8L825.9 109c4.1-5.3.4-13-6.3-13H436.1c-2.8 0-5.5 1.5-6.9 4L170.1 547.5c-3.1 5.3.7 12 6.9 12h174.4L262 917.1c-1.9 7.8 7.5 13.3 13.3 7.7L853.6 373c5.2-4.9 1.7-13.7-5.5-13.7M378.3 732.5l60.3-241H281.2l189.6-327.4h224.6L487.1 427.4h211z" /></svg>;
 };
 
 const ProjectHighlightCard = ({
@@ -21,7 +21,7 @@ export function ProjectHighlightsMarquee({ containerWidth, highlights }: { conta
   const width = containerWidth ? containerWidth : 390;
 
   return (
-    <div className={`relative flex flex-col justify-center items-center w-[${width}px] h-[75px]`}>
+    <div className={`relative flex flex-col justify-center items-center w-full md:w-[${width}px] h-[75px]`}>
       <div className='flex flex-col gap-2 bg-base-3000 h-full'>
         <Marquee className='[--duration:25s]'>
           {highlights.map((highlight, index) => (
